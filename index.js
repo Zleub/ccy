791a630e75446c80236e1c915dd334ebed2845ee
Arnaud DEBRAY <debray.arnaud@gmail.com>
2018-02-21 23:42:23 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const https = require('https')
+const child_process = require('child_process')
 
 let options = {
 	"method": 'POST',
@@ -12,219 +13,84 @@ let options = {
 
 let {default: Binance} = require('binance-api-node')
 
-// const client = Binance()
+const client = Binance()
+
+if (process.argv.slice(2, 5).length != 3) {
+	process.exit()
+}
+
+let monals = process.argv.slice(2, 5)
+
+let corrs = {}
+corrs[monals[0] + monals[1]] = monals[1] + monals[0]
+corrs[monals[0] + monals[2]] = monals[2] + monals[0]
+corrs[monals[1] + monals[2]] = monals[2] + monals[1]
+
+let splits = {}
+let comple = {}
+for (var i = 0; i < monals.length; i++) {
+	for (var j = 0; j < monals.length; j++) {
+		for (var k = 0; k < monals.length; k++) {
+			if (monals[i] != monals[j])
+				splits[monals[i] + monals[j]] = [monals[i], monals[j]]
+				if (monals[i] != monals[k] && monals[j] != monals[k])
+					comple[monals[i] + monals[j]] = monals[k]
+			}
+	}
+}
+
 
 // Authenticated client, can make signed calls
-const client = Binance({
-  apiKey: '80CAh6CWHNmfDCNgAZ6BGaUAolHUicWCKnhpLCLHu21gsoextvoymLP09Z2ytScw',
-  apiSecret: 'yDHV58Y68rsxhrs5y7USdwV1YWovxj5V0pVjCh2fKgZ6PEHJ82oiEbxW2CvrZRNB',
-})
-
-// console.log(client)
-
-// client.time().then(t => console.log(`start: ${new Date(t)}`))
-// client.accountInfo().then( console.log )
-// client.exchangeInfo().then( e => {
-// 	e.symbols.forEach(_ => {
-// 		if (_.baseAsset == 'ETH' && _.quoteAsset == 'BTC') {
-// 			console.log(_)
-// 			// console.log(`${_.baseAsset} -> ${_.quoteAsset}`)
-// 			// console.log(_.filters)
-// 		}
-// 	})
+// const client = Binance({
+  // apiKey: '80CAh6CWHNmfDCNgAZ6BGaUAolHUicWCKnhpLCLHu21gsoextvoymLP09Z2ytScw',
+  // apiSecret: 'yDHV58Y68rsxhrs5y7USdwV1YWovxj5V0pVjCh2fKgZ6PEHJ82oiEbxW2CvrZRNB',
 // })
+client.exchangeInfo().then( exchange => {
 
-let corrs = {
-	'LTCBNB' : 'BNBLTC', //: 'LTCBNB',
-	'LTCUSDT': 'USDTLTC',
-	'BNBUSDT': 'USDTBNB',
-	// 'BNBUSDT': 'USDTBNB',
-}
+	corrs = Object.keys(corrs).reduce((p,k) => {
+		if (!exchange.symbols.some(e => e.symbol == k))
+			p[ corrs[k] ] = k
+		if (!exchange.symbols.some(e => e.symbol == corrs[k]))
+			process.exit(-1)
+		return p
+	}, {})
 
-let splits = {
-	'BNBLTC': ['BNB','LTC'],
-	'LTCUSDT': ['LTC','USDT'],
-	'BNBUSDT': ['BNB','USDT'],
-	'LTCBNB': ['LTC','BNB'],
-	'USDTLTC': ['USDT','LTC'],
-	'USDTBNB': ['USDT','BNB'],
-	// 'BNBUSDT': ['BNB','USDT']
-}
-
-let comple = {
-	'BNBLTC': 'USDT',
-	'LTCUSDT': 'BNB',
-	'BNBUSDT': 'LTC',
-	'LTCBNB': 'USDT',
-	'USDTLTC': 'BNB',
-	'USDTBNB': 'LTC',
-	// 'BNBUSDT': ''
-}
-
-let f = array => {
-	console.log( new Date().toTimeString() )
-	let fs = {}
+	console.log([corrs, splits, comple])
 
 
-	// client.book({ symbol: 'ETHBTC' }).then( book => {
-	// 	// console.log('book', book)
-    //
-	// 	client.ws.trades(array, e => {
-	// 		let date = new Date(e.eventTime)
-	// 		// if (e.maker)
-	// 		console.log(date, 'trades')//, e.price, e.quantity, e.maker)
-	// 	})
-    //
-	// 	client.ws.depth(array, e => {
-	// 		let date = new Date(e.eventTime)
-	// 		console.log(date, 'depth')//, e)
-	// 	})
-    //
-	// 	setTimeout( () => {
-	// 		client.ws.depth(array, e => {
-	// 			let date = new Date(e.eventTime)
-	// 			console.log(date, 'depth')//, e)
-	// 		})
-	// 	}, 200 )
-
-		// client.ws.partialDepth({ symbol: 'ETHBTC', level: 10 }, e => {
-		// 	let date = new Date(e.eventTime)
-		// 	console.log(new Date(), 'partialDepth')//, e)
-		// })
-        //
-		// client.ws.ticker(array, e => {
-		// 	let date = new Date(e.eventTime)
-		// 	console.log(date, 'ticker')//, e.bestBid, e.bestAsk)
-		// })
-	// } )
+	let f = array => {
+		console.log( new Date().toTimeString() )
 
 
-	// array.forEach(a => {
+		let fs = {}
 		let count = 0
 		let close = client.ws.ticker(array, e => {
 			let date = new Date(e.eventTime)
-			// console.log(e)
-			// console.log(date.toTimeString(), e)
-			// console.log(`[${e.date}] ${e.symbol} : ${e.priceChange} {${e.curDayClose}}`)
-			// console.log( JSON.stringify(e, false, "  ") )
-			// client.prices().then(e => console.log( JSON.stringify(e['NANOETH'], false, "  ")))
-			// console.log('----------------------------------------------------------')
-			// caca()
-			// if (e.maker)
 			fs[ e.symbol ] = x => x * e.bestBid
 			fs[ corrs[e.symbol] ] = x => x * (1 / e.bestAsk)
 
 			Object.keys(fs).forEach(k => {
 				if (fs[splits[k][1] + comple[k]] && fs[comple[k] + splits[k][0]]) {
-
-					if (fs[comple[k] + splits[k][0]](fs[splits[k][1] + comple[k]](fs[k](1))) > 1.005) {
+					if (fs[comple[k] + splits[k][0]](fs[splits[k][1] + comple[k]](fs[k](1))) > 1.005 ) {
 						let [x, y, z] = [
 							Number(fs[k](1).toFixed(8)),
 							Number((fs[splits[k][1] + comple[k]]( Number(fs[k](1).toFixed(8)) ).toFixed(8))),
 							Number((fs[comple[k] + splits[k][0]]( Number((fs[splits[k][1] + comple[k]]( Number(fs[k](1).toFixed(8)) ).toFixed(8))) ).toFixed(8)))
 						]
-						exec(`curl -X POST -H 'Content-type: application/json' --data '{"text":${date.toTimeString()} 1 ${splits[k][0]} -> ${x}
-	${splits[k][1]} -> ${y}
-	${comple[k]} -> ${z} ${splits[k][0]}}' https://hooks.slack.com/services/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`)
+						let str = `${date.toTimeString()} 1 ${splits[k][0]} -> ${x} ${splits[k][1]} -> ${y} ${comple[k]} -> ${z} ${splits[k][0]}`
+						str += Object.keys(e).reduce((p,k) => p + `${k}: ${e[k]}\n`, " ```") + '```'
+						// child_process.exec(`curl -X POST -H 'Content-type: application/json' --data '{"text": "${str}"}' https://hooks.slack.com/services/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`, (err, stdout, stderr) => {
+						console.log(str)
+						// })
 
-						console.log(`${date.toTimeString()} 1 ${splits[k][0]} -> ${x}
-	${splits[k][1]} -> ${y}
-	${comple[k]} -> ${z} ${splits[k][0]}`)
 					}
 				}
 			})
-    //
-	// 		count += 1
-			// if (count > 20) {
-				// close()
-				// console.log( fs )
-			// }
 		})
-	// })
 
-}
+	}
 
-f(Object.keys(corrs))
-// client.ws.trades(array, e => {
+	f(Object.keys(corrs))
 
-// f('ETH')('BTC')
 
-let currs = [
-	'ETH',
-	'BTC',
-	// 'ADA',
-	// 'BNB',
-	'USDT',
-	// 'LTC',
-	// 'NEO',
-	// 'BCC'
-]
-
-// let caca = () => client.exchangeInfo().then( e => {
-// 	let count = 0
-// 	let {symbols} = e
-//
-// 	let tmp = []
-// 	symbols.forEach(e => {
-// 		let {symbol, baseAsset: base, quoteAsset: quote} = e
-// 		if (currs.find( _ => _ == e.baseAsset ) && currs.find( _ => _ == e.quoteAsset )) {
-// 			client.prices().then(e => {
-// 				e = Object.keys(e).reduce( ((p,k) => {p[k] = Number(e[k]) ; return p}), {} )
-// 				currs.forEach( c => {
-// 					if (c != base && c != quote) {
-// 						console.log(`base: ${base} | quote: ${quote} | c : ${c}`)
-// 						let a = {
-// 							[base + quote] : e[base + quote],
-// 							[quote + base] : Number((1 / e[base + quote]).toFixed(8)),
-// 							[c + quote] : e[c + quote] || Number((1 / e[quote + c]).toFixed(8)),
-// 							[c + base] : e[c + base] || Number((1 / e[base + c]).toFixed(8)),
-// 							[quote + c] : e[quote + c] || Number((1 / e[c + quote]).toFixed(8)),
-// 							[base + c] : e[base + c] || Number((1 / e[c + quote]).toFixed(8)),
-// 						}
-//
-// 						console.log(a)
-// 					}
-// 				})
-//
-// 			})
-//
-// 		}
-// 	})
-// })
-
-// let g = a => b => client.prices().then( e => {
-// 	console.log(e)
-// 	// if (e[a + b])
-// 	// 	console.log(`${a} -> ${b}: ${e[a + b]}`)
-// 	// console.log(`${b} -> ${a}: ${e[b + a]}`)
-// 	// f(a)(b)
-// 	// f(b)(a)
-// })
-//
-// let currencies = [ 'BTC', 'ETH', 'USDT', 'BNB', 'ADA' ]
-//
-// currencies.forEach( (e,i) => {
-// 	currencies.forEach( (_e, _i) => {
-// 		if (i != _i)
-// 			g(e)(_e)
-// 	})
-// })
-
-// g('ETH')('BTC')
-// g('ETH')('BNB')
-//
-// g('BTC')('ETH')
-// g('BTC')('USDT')
-//
-// g('USDT')('ETH')
-// g('USDT')('BTC')
-// global.sum = []
-//
-// let sum = x => y => x + y
-// for (var i = 0; i < 9; i++) {
-// 	global['sum'].push(sum(i))
-// }
-//
-// console.log(global.sum)
-// console.log(global.sum[1](3))
-// console.log(global.sum[4](4))
+})
---
+++ spawner.js
@@ -0,0 +1,63 @@
+const https = require('https')
+const {spawn} = require('child_process')
+
+let options = {
+	"method": 'POST',
+	"host": 'api.binance.com',
+	"path": '/api/v3/account',
+
+	headers: {
+		"X-MBX-APIKEY": "80CAh6CWHNmfDCNgAZ6BGaUAolHUicWCKnhpLCLHu21gsoextvoymLP09Z2ytScw"
+	}
+}
+
+let {default: Binance} = require('binance-api-node')
+
+const client = Binance()
+
+let dict = {}
+client.exchangeInfo().then( exchange => {
+	exchange.symbols.forEach(e => {
+		dict[e.quoteAsset] = true
+	})
+	exchange.symbols.forEach(e => {
+		dict[e.baseAsset] = true
+	})
+
+	let __ = Object.keys(dict).slice(0, 32).filter(e => e != '123' && e != '456')
+	console.log(__)
+
+	__.forEach( i => {
+	__.forEach( j => {
+	__.forEach( k => {
+		if (i != j && i != k && j != k) {
+			const args = ['index.js', i, j, k]
+			const child = spawn('node', args);
+
+			child.stdout.on('data', (data) => {
+			  console.log(`stdout: ${data}`);
+			});
+
+			child.stderr.on('data', (data) => {
+			  console.log(`stderr: ${data}`);
+			});
+
+			child.on('close', (code) => {
+			  console.log(`child process exited with code ${code}`);
+			});
+
+			child.on('exit', (code) => {
+			  console.log(`child process exited with code ${code}`);
+			});
+			child.on('disconnect', (code) => {
+			  console.log(`child process exited with code ${code}`);
+			});
+
+			child.on('error', (err) => {
+			  console.log(`Failed to start subprocess ${args}`);
+			});
+		}
+	})})})
+
+	// while (42) ;
+})
